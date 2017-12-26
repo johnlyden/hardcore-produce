6a36e809c7ea4a32aa812ac967e6ae49226dc8a3
John Lyden <johnlydennc@gmail.com>
2017-12-26 15:27:13 -0600

    added sorting of scores before html creation

--- js/script.js
+++ js/script.js
@@ -28,13 +28,18 @@
      * @param { Array } scores 
      */
     updateLeaderBoard(scores) {
-      let htmlString = ''
-      scores.map( (score) => {
+      let htmlString = '';
+      
+      const sortedScores = scores.sort( (a, b) => {
+        return parseFloat(b.count) - parseFloat(a.count)
+      });
+
+      sortedScores.map( (score) => {
         htmlString += `<li class="score-card"><span class="name">${score.name}</span><span class="score pull-right">${score.count} Mentions</span></li>`;
       });
-      console.log(htmlString);
+
       this.leaderBoard.innerHTML = htmlString;
-    }
+    } 
 
   }
 
