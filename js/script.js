849cb5eae7ade09bf7c03fee40bcef8d96778bf4
John Lyden <johnlydennc@gmail.com>
2017-12-26 20:25:25 -0600

    added comments - final styling

--- js/script.js
+++ js/script.js
@@ -1,47 +1,59 @@
 (function() {
 
+  /** class representing the leaderboard  */
   class LeaderBoard {
     constructor() {
+      // create new instance of poller
       this.poller = new window.spredfast.Poller();
-      this.init();
       this.leaderBoard = document.getElementById('leaderBoard');
+      this.init();
     }
 
+    /**
+     * initialize the leaderboard
+     */
     init() {
-      this.setPolling();
+      this.setPolling(15000);
+      this.requestUpdate();
     }
 
-    setPolling() {
+    /**
+     * set interval to start polling the API
+     * @param {number} interval 
+     */
+    setPolling(interval) {
       setInterval( () => {
         this.requestUpdate();
-      }, 3000);
+      }, interval);
     }
 
+    /**
+     * call the poll method and handle response
+     */
     requestUpdate() {
       this.poller.poll().then( (resp) => {
         this.updateLeaderBoard(resp);
       })
     }
+
     /**
-     * 
+     * Iterates through scores, creates HTML and injects into DOM
      * @param { Array } scores 
      */
     updateLeaderBoard(scores) {
-      
       let htmlString = '';
+
       const sortedScores = scores.sort( (a, b) => {
         return parseFloat(b.count) - parseFloat(a.count)
       });
 
       sortedScores.map( (score) => {
-        htmlString += `<li class="score-card animated slideInLeft"><span class="name">${score.name}</span><span class="score pull-right">${score.count} Mentions</span></li>`;
+        htmlString += `<li class="score-card animated flipInX"><span class="name">${score.name}</span><span class="score pull-right">${score.count} <span class="mention">Mentions</span></span></li>`;
       });
 
       this.leaderBoard.innerHTML = htmlString;
     } 
 
   }
-
   const board = new LeaderBoard();
-
 })();
